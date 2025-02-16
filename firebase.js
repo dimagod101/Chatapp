adcbe624717e40351488c11420cb66231bb06bff
dimagod101 <65247557+dimagod101@users.noreply.github.com>
2025-02-16 14:15:00 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC72WABeQ2e117WwxS7BAs_L1A2cIo0u0Y",
+    authDomain: "messagingapp-9bac7.firebaseapp.com",
+    databaseURL: "https://messagingapp-9bac7-default-rtdb.firebaseio.com/",
+    projectId: "messagingapp-9bac7",
+    storageBucket: "messagingapp-9bac7.appspot.com",
+    messagingSenderId: "369463204028",
+    appId: "1:369463204028:web:a932d813be7ac41f5b98b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const messagesRef = ref(db, "messages");
+
+// Export Firebase functions
+export { db, messagesRef, ref, push, onValue, remove };
